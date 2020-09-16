8a2636135666f5e402ef161ae0356f07980c0f38
Александр Захаров <a.zaharov@transimperial.local>
2020-09-16 17:23:59 +0300

    add API

--- src/app.js
+++ src/app.js
@@ -2,14 +2,37 @@
 window.addEventListener('load', ()=>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone')
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
-           long = ppsition.coords.longitude;
+           long = position.coords.longitude;
            lat = position.coords.latitude;
-           //13.43
+
+           const apiKey = '7ed9d435b9ac07fc6d9c5c2033420a51';
+           const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`;
+            
+           fetch(api)
+                .then(response =>{
+                    return response.json();
+                })
+                .then(data=>{
+                    console.log(data);
+                    const {temperature, summary} = data.currently;
+                    data.currently.temperature
+                    //set DOM Elements from the API
+
+        })
+        
         })
     }else {
-        h1. textContent = 'hey, not working, i dont know why(( Sorry'
+        h1.textContent = 'hey, not working, i dont know why(( Sorry'
     }
 })
+
+/*
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`
+*/
